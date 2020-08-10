ab2bd267fbd87f429f7097d32f119c9841dcbb2a
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-08-10 22:18:50 +0530

    implemented channing mechanism concept

---
+++ In HTML file/Scripts/channing_mechanism.js
@@ -0,0 +1,13 @@
+/// >reference path = "angular.min.js"/>
+
+
+var myApp = angular
+                .module("myModule", []) 
+                .controller("myController", function($scope) {   
+                    var employee = {
+                        firstName: "Gagandeep",
+                        lastName: "Kaur",
+                        gender: "Female"
+                    }
+                    $scope.employee = employee;
+                });
